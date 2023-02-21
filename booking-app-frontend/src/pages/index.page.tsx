4a5868a5a0de9d9791a23ccdd48c48cbdfb5074a
matheusjustino <matheus.justino@ccc.ufcg.edu.br>
2023-02-21 18:38:17 -0300

    image

--- booking-app-frontend/src/pages/index.page.tsx
+++ booking-app-frontend/src/pages/index.page.tsx
@@ -24,13 +24,16 @@ const HomePage: React.FC = () => {
 					>
 						<div className="rounded-2xl h-[180px] mb-2">
 							{place.photos[0] && (
-								<Image
-									className={`rounded-2xl aspect-square ${styles.imagePreview}`}
+								// <Image
+								// 	className={`rounded-2xl aspect-square ${styles.imagePreview}`}
+								// 	src={`${process.env.NEXT_PUBLIC_BASE_URL}/places/images/${place.photos[0]}`}
+								// 	layout="fill"
+								// 	alt="Preview"
+								// />
+								<img
+									className={`rounded-2xl aspect-square h-full w-full inset-0 bg-transparent ${styles.imagePreview}`}
 									src={`${process.env.NEXT_PUBLIC_BASE_URL}/places/images/${place.photos[0]}`}
-									layout="fill"
 									alt="Preview"
-									// width={200}
-									// height={250}
 								/>
 							)}
 						</div>
